89597669d7e58674d3219c10135b2b02a895458e
camigonzalez568 <cg3857722@gmail.com>
2023-12-11 09:29:33 -0300

    chau

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = 4400;
+const port = 5500;
 
 // Conectar MongoDB
-mongoose.connect('mongodb+srv://clgonzalez:48537645aA@clusterr.xffnu0h.mongodb.net/', {
+mongoose.connect('mongodb+srv://clgonzalez:<48537645>@cluster0.svwfvnx.mongodb.net/', {
 
 })
     .then(() => {
