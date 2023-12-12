866fe090294379985477aea74b1481b6f06e7f66
camigonzalez568 <cg3857722@gmail.com>
2023-12-12 12:00:06 -0300

    br

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const miBoton = document.getElementById('miBoton');
+const mongoose = require('mongoose');
+
+const schema = datos.object({
+    username : String,
+    surname  :  String,
+     age     :  Number,
+});
+
+
+module.exports = datos;
 
--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
+const datos = require('./schema'); 
 const port = 3000;
 
+const data = {
+    username : String,
+    surname  :  String,
+     age     :  Number,
+  };
+
+app.post('/datos', (req,res) => {
+    console.log(req.body);
+    res.send('Datos recibidos con exitoo')
+});
 
 mongoose.connect('mongodb+srv://clgonzalez:1234@cluster0.nkl5shc.mongodb.net/', {
     useNewUrlParser: false,
